652ff6af00634cabc9e5b0f215242754cdb8f25a
Reiky <32432650+Toveyrui@users.noreply.github.com>
2023-11-19 17:32:13 +0800

    Initial commit

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Gamble_can_can' 
+ * Target:  'Gamble_can_can' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
